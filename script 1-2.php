e362f7fc40d81eff3f3315831ec6431ecb2700fa
ULADZISLAU YANCHEUSKI <tinirait@gmail.com>
2020-07-25 10:51:21 +0300

    Cahnge script_2 - random

--- script 1-2.php
+++ script 1-2.php
@@ -34,35 +34,21 @@ $objectSecond->getLetter();
 
 
 
- echo "<br>";
- echo "N2";
+echo "<br>";
+echo "N2";
 
-$colorArr = ['#FF0000', '#1E90FF', '#228B22', '#FFFF00', '#00FF00', '#FF00FF', '#000000', '#FFD700', '#808080', '#DC143C'];
 $color = ['red', 'blue', 'green', 'yellow', 'lime', 'magenta', 'black', 'gold', 'grey', 'tomato'];
-
-
-$randomNumber = rand(0, 9);
-$randomColor = rand(0, 9);
 
 for ($i = 0; $i < 25; $i++) {
     if ($i % 5 == 0) {
         print("<br/>");
     }
-    echo "<span style='color:$colorArr[$randomColor]'>$color[$randomNumber] </span>";
-    //Переопределение счетчиков
-    $randomColor = rand(0, 9);
-    $randomNumber = rand(0, 9);
+    do {
+        $random1 = rand(0,9);
+        $random2 = rand(0,9);
+    }while( $random1 == $random2);
+
+    print ("<span style='color:$color[$random1]'>$color[$random2] </span>");
 }
 
 
-
-//let str = '1 -2 -3 4 5 -6f ss3 0 0 0 -0 0.0 0.05';
-//
-//    function strNatural(str) {
-//        let newStr = str.replace(/\D/g, "").split(/\s*/).sort().join("");
-//
-//        console.log(newStr);
-//
-//    };
-//
-//    strNatural(str);
